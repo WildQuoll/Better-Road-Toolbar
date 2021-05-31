27ee0d3f999f2d9964b64adedfa5d48c32b5b8d0
WildQuoll <>
2021-05-31 14:01:41 +0100

    Minor code re-organisation to improve compatibility with NExt 2.

--- BetterRoadToolbar/SpawnButtonEntryPatch.cs
+++ BetterRoadToolbar/SpawnButtonEntryPatch.cs
@@ -2,42 +2,60 @@
 using HarmonyLib;
 using System;
 using UnityEngine;
+using static GeneratedGroupPanel;
 
 namespace BetterRoadToolbar
 {
-	[HarmonyPatch(typeof(GeneratedGroupPanel), "SpawnButtonEntry",
-		new Type[] { typeof(UITabstrip), typeof(string), typeof(string), typeof(bool), typeof(string), typeof(string), typeof(string), typeof(bool), typeof(bool) })]
+	// Alternatively could patch GeneratedGroupPanel.SpawnButtonEntry, but NExt2 redirects that method, so that wouldn't always work.
+	[HarmonyPatch(typeof(GeneratedGroupPanel), "PopulateGroups", new Type[] { typeof(GroupFilter), typeof(Comparison<GroupInfo>) })]
 	class SpawnButtonEntryPatch
 	{
 		[HarmonyPostfix]
-		public static void Postfix(UITabstrip strip, string name, string category, bool isDefaultCategory, string localeID, string unlockText, string spriteBase, bool enabled, bool forceFillContainer, ref UIButton __result)
+		public static void Postfix(GroupFilter filter, Comparison<GroupInfo> comparison, GeneratedGroupPanel __instance, UITabstrip ___m_Strip)
 		{
+			if (!(__instance is RoadsGroupPanel))
+            {
+				// We only want the "Roads" main tab
+				return;
+            }
+
 			string mainCategoryId = "MAIN_CATEGORY";
 
-			if (localeID == mainCategoryId && __result.tooltip.Contains(Mod.Identifier))
+			foreach (var tab in ___m_Strip.tabs)
             {
-				string s = __result.tooltip.Replace(mainCategoryId + "[" + Mod.Identifier, "");
-				s = s.Replace("]:0", "");
+				var button = tab as UIButton;
 
-				int val;
-				bool result = int.TryParse(s, out val);
-
-				if (!result)
-				{
-					Debug.Log(Mod.Identifier + "Unable to parse string: '" + __result.tooltip + "'");
-					return;
-				}
-
-				RoadCategory cat = (RoadCategory)val;
-
-				if(!Enum.IsDefined(typeof(RoadCategory), cat))
+				if(!button)
                 {
-					Debug.Log(Mod.Identifier + "Unexpected RoadCategory value: '" + val + "'");
-					return;
-				}
+					// shouldn't happen?
+					continue;
+                }
 
-				__result.tooltip = RoadAnalyser.GetTooltip(cat);
-				__result.text = RoadAnalyser.GetToolbarTitle(cat);
+				if(button.tooltip.Contains(Mod.Identifier))
+                {
+					string s = button.tooltip.Replace(mainCategoryId + "[" + Mod.Identifier, "");
+					s = s.Replace("]:0", "");
+
+					int val;
+					bool result = int.TryParse(s, out val);
+
+					if (!result)
+					{
+						Debug.Log(Mod.Identifier + "Unable to parse string: '" + button.tooltip + "'");
+						return;
+					}
+
+					RoadCategory cat = (RoadCategory)val;
+
+					if (!Enum.IsDefined(typeof(RoadCategory), cat))
+					{
+						Debug.Log(Mod.Identifier + "Unexpected RoadCategory value: '" + val + "'");
+						return;
+					}
+
+					button.tooltip = RoadAnalyser.GetTooltip(cat);
+					button.text = RoadAnalyser.GetToolbarTitle(cat);
+				}
             }
 		}
 	}
